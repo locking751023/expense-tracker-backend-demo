b4b5fb378c801b688adbc604ca7dccfd82b58762
Tommy1023 <j.h.chen1023@gmail.com>
2022-09-26 22:17:24 +0800

    refactor user api

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,34 +3,33 @@ const { User } = require('../models')
 const jwt = require('jsonwebtoken')
 
 const userController = {
-  register: (req, res, next) => {
-    const { name, email, password, checkPassword } = req.body
-    if (!name || !email || !password || !checkPassword ) return res.status(400).json({ status: 'error', message: '所有欄位為必填!'})
-    if (password !== checkPassword) return res.status(400).json({ status: 'error', message: '請確認密碼輸入一致！' })
-    User.findOne({ where: { email } })
-      .then(user => {
-        if (user) return res.status(400).json({ status: 'error', message: 'email 已被註冊！' })
-        return bcrypt.hash(password, 10)
-      })
-      .then(hash => User.create({
+  register: async (req, res, next) => {
+    try {
+      const { name, email, password, checkPassword } = req.body
+      if (!name || !email || !password || !checkPassword ) return res.status(400).json({ status: 'error', message: '所有欄位為必填!'})
+      if (password !== checkPassword) return res.status(400).json({ status: 'error', message: '請確認密碼輸入一致！' })
+      const user = await User.findOne({ where: { email } })
+      if (user) return res.status(400).json({ status: 'error', message: 'email 已被註冊！' })
+      const hash = await bcrypt.hash(password, 10)
+      const newUser = await User.create({
           name,
           email,
           password: hash
-      }))
-      .then((result) => {
-        const user = {
-          ...result.toJSON()
-        }
-        delete user.password
-        res.status(201).json({ status: 'success', message: '帳號註冊成功！', user})
       })
-      .catch(err => next(err))
+      const removePassword = {
+        ...newUser.toJSON(),
+      }
+      delete removePassword.password
+      res.status(201).json({ status: 'success', message: '帳號註冊成功！', removePassword})
+    } catch (err) {
+      next(err)
+    }
   },
-  login: (req, res, next) => {
+  login: async (req, res, next) => {
     try {
       const userData = req.user.toJSON()
       delete userData.password
-      const token = jwt.sign(userData, process.env.JWT_SECRET)
+      const token = await jwt.sign(userData, process.env.JWT_SECRET)
       res.status(200).json({
         status: 'success',
         data: {
