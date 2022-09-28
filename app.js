9ae6352bc21410a6ece0e0f051adc117eae27302
Tommy1023 <j.h.chen1023@gmail.com>
2022-09-28 17:10:11 +0800

    require cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const passport = require('passport')
 
 if (process.env.NODE_ENV !== 'production') {
@@ -9,6 +10,15 @@ const app = express()
 const PORT = process.env.PORT
 const api = require('./routers/index')
 
+const corsOptions = {
+  origin: [
+    'http://localhost:3001',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true })) 
 app.use(express.json()) 
 app.use(passport.initialize())
